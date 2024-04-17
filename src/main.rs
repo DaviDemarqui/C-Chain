716c5198b0c31c9f1738aac0b7f880f745495e20
DaviDemarqui <davicoding@protonmail.com>
2024-04-17 00:15:48 -0300

    Block struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+
+struct Block {
+    pub id: u64,
+    pub data: String,
+    pub timestamp: i64,
+    pub hash: String,
+    pub previous_hash: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
